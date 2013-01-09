ed8760c265ff69ab08d1a1b72b644e8a281b0d0e
jfu <jfu@jfu-desktop.goconnrnd.goconnect.com.au>
2013-01-09 13:08:51 +1100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'd251a8bd30e5ea7076d3f1808f26b98a063fb4549e089728094fac4a6d2119fe35953eb5119dee38864cd5ce8f4a0757503ca690eb19f65e0469214a2d6580b7'
