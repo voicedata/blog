7c3473e5b22afb941fe6dc15334eb213ea13f265
jfu <jfu@jfu-desktop.goconnrnd.goconnect.com.au>
2013-01-17 09:22:50 +1100

    use constant for directory

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -21,7 +21,8 @@ worker_processes 2
 
 # Help ensure your application will always spawn in the symlinked
 # "current" directory that Capistrano sets up.
-working_directory "/opt/www/blog/current" # available in 0.94.0+
+CURRENT_DIRECTORY = "/opt/www/blog/current"
+working_directory CURRENT_DIRECTORY # available in 0.94.0+
 
 user "www-user", "www-data"
 # listen on both a Unix domain socket and a TCP port,
@@ -33,14 +34,14 @@ listen 8080, :tcp_nopush => true
 timeout 30
 
 # feel free to point this anywhere accessible on the filesystem
-pid "#{working_directory}/tmp/pids/unicorn.pid" 
+pid "#{CURRENT_DIRECTORY}/tmp/pids/unicorn.pid" 
 
 
 # By default, the Unicorn logger will write to stderr.
 # Additionally, ome applications/frameworks log to stderr or stdout,
 # so prevent them from going to /dev/null when daemonized here:
-stderr_path "#{working_directory}/log/unicorn.stderr.log"
-stdout_path "#{working_directory}/log/unicorn.stdout.log"
+stderr_path "#{CURRENT_DIRECTORY}/log/unicorn.stderr.log"
+stdout_path "#{CURRENT_DIRECTORY}/log/unicorn.stdout.log"
 
 # combine Ruby 2.0.0dev or REE with "preload_app true" for memory savings
 # http://rubyenterpriseedition.com/faq.html#adapt_apps_for_cow
